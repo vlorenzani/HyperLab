918ec2db6e9472f9674cc844b73e5c61b08be4a8
vlorenzani <v.lorenzani@gmail.com>
2021-07-26 09:26:04 +0200

    Create Script 1 Domain.ps1

---
+++ Active Directory/Domain/Script 1 Domain.ps1
@@ -0,0 +1,18 @@
+﻿$Secure_String_Pwd = ConvertTo-SecureString "YourPassword" -AsPlainText -Force
+
+$Params = @{
+    CreateDnsDelegation = $false
+    DatabasePath = 'E:\7\NTDS'
+    DomainMode = '7'
+    DomainName = 'YourDomain'
+    DomainNetbiosName = 'YourDomain'
+    ForestMode = '7'
+    InstallDns = $true
+    LogPath = 'E:\Windows\NTDS'
+    NoRebootOnCompletion = $true
+    SafeModeAdministratorPassword = $Secure_String_Pwd
+    SysvolPath = 'E:\Windows\SYSVOL'
+    Force = $true
+}
+ 
+Install-ADDSForest @Params
